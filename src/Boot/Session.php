c6aabf66928892f21b5bc58e642bf061da842056
TOBENTOch <info@tobento.ch>
2023-04-23 10:20:56 +0200

    previous uri added and made some changes

--- src/Boot/Session.php
+++ src/Boot/Session.php
@@ -14,11 +14,13 @@ declare(strict_types=1);
 namespace Tobento\App\Http\Boot;
 
 use Tobento\App\Boot;
+use Tobento\App\Boot\Config;
 use Tobento\App\Http\Boot\Middleware;
 use Tobento\App\Http\SessionFactory;
 use Tobento\Service\Session\SessionInterface;
-use Tobento\Service\Config\ConfigInterface;
-use Tobento\Service\Config\ConfigLoadException;
+use Tobento\Service\Uri\PreviousUriInterface;
+use Tobento\Service\Uri\PreviousUri;
+use Psr\Http\Message\UriFactoryInterface;
 
 /**
  * Session boot.
@@ -33,43 +35,50 @@ class Session extends Boot
     ];
     
     public const BOOT = [
+        Config::class,
         Middleware::class,
     ];
     
     /**
      * Boot application services.
      *
+     * @param Config $config
      * @param Middleware $middleware
      * @return void
      */
-    public function boot(Middleware $middleware): void
+    public function boot(Config $config, Middleware $middleware): void
     {
-        $config = $this->app->get(ConfigInterface::class);
-
-        try {            
-            $config->load('session.php', 'session');
-        } catch (ConfigLoadException $e) {
-            // ignore
-        }
+        $config = $config->load('session.php');
         
         $this->app->set(SessionInterface::class, function() use ($config): SessionInterface {
-
-            $sessionFactory = $this->app->make($config->get('session.factory', SessionFactory::class));
+            
+            $sessionFactory = $this->app->make($config['factory'] ?? SessionFactory::class);
                         
             $session = $sessionFactory->createSession(
-                $config->get('session.name', 'sess'),
-                $config->get('session.config', []),
+                $config['name'] ?? 'sess',
+                $config['config'] ?? [],
             );
             
             return $session;
         });
         
-        $middleware->add(
-            $config->get(
-                'session.middleware',
-                \Tobento\Service\Session\Middleware\Session::class
-            ),
-            priority: 6000
-        );
+        // Handle previous uri:
+        $this->app->on(PreviousUriInterface::class, function(PreviousUriInterface $prevUri) {
+            
+            $session = $this->app->get(SessionInterface::class);
+
+            if ($session->has('_previous_uri')) {
+                $uriFactory = $this->app->get(UriFactoryInterface::class);
+                $uri = $uriFactory->createUri($session->get('_previous_uri'));
+                return new PreviousUri($uri);
+            }
+            
+            return $prevUri;
+        });
+
+        // adding middlewares:
+        foreach($config['middlewares'] ?? [] as $mw) {
+            $middleware->add($mw, priority: 6000);
+        }
     }
 }
