f86e97aca1822e8f16b08f707ac15fa488a4829d
TOBENTOch <info@tobento.ch>
2023-08-18 17:32:11 +0200

    initial commit

---
+++ tests/Boot/CustomErrorHandlerTest.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+
+namespace Tobento\App\Http\Test\Boot;
+
+use PHPUnit\Framework\TestCase;
+use Tobento\App\AppInterface;
+use Tobento\App\AppFactory;
+use Tobento\App\Http\Boot\Http;
+use Tobento\App\Http\Boot\Routing;
+use Tobento\App\Http\ResponseEmitterInterface;
+use Tobento\App\Http\Test\TestResponse;
+use Tobento\App\Http\Test\Mock\ResponseEmitter;
+use Tobento\App\Http\Test\Mock\CustomErrorHandler;
+use Tobento\Service\Session\RemoteAddrValidation;
+use Tobento\Service\Session\SessionValidationException;
+use Tobento\Service\Filesystem\Dir;
+use Psr\Http\Message\ServerRequestInterface;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Tobento\App\Http\Boot\ErrorHandler;
+
+/**
+ * CustomErrorHandlerTest
+ */
+class CustomErrorHandlerTest extends TestCase
+{
+    protected function createApp(
+        array $request = [],
+        null|string $accept = null,
+        bool $deleteDir = true,
+    ): AppInterface {
+        if ($deleteDir) {
+            (new Dir())->delete(__DIR__.'/../app/');
+        }
+        
+        (new Dir())->create(__DIR__.'/../app/');
+        (new Dir())->create(__DIR__.'/../app/config/');
+        
+        $app = (new AppFactory())->createApp();
+        
+        $app->dirs()
+            ->dir(realpath(__DIR__.'/../app/'), 'app')
+            ->dir($app->dir('app').'config', 'config', group: 'config');
+        
+        // Replace response emitter for testing:
+        $app->on(ResponseEmitterInterface::class, ResponseEmitter::class);
+        
+        $app->on(ServerRequestInterface::class, function() use ($request, $accept) {            
+            $serverRequest = (new Psr17Factory())->createServerRequest(...$request);
+            
+            if ($accept) {
+                return $serverRequest->withAddedHeader('Accept', $accept);
+            }
+            
+            return $serverRequest;
+        });
+        
+        $app->boot(CustomErrorHandler::class);
+        $app->boot(Routing::class);
+        $app->booting();
+
+        $app->route('GET', '', function() {
+            return 'home';
+        })->name('home');
+        
+        $app->route('GET', 'unsubscribe/{user}', function() {
+            return 'unsubscribed';
+        })->signed('unsubscribe');
+        
+        return $app;
+    }
+    
+    public static function tearDownAfterClass(): void
+    {
+        (new Dir())->delete(__DIR__.'/../app/');
+    }
+    
+    public function testAnyExceptionWithMessage()
+    {
+        $app = $this->createApp(request: [
+            'method' => 'GET',
+            'uri' => '',
+            'serverParams' => [],
+        ]);
+        
+        $app->middleware(function($request, $handler) {
+            throw new \RuntimeException();
+        });
+                
+        $app->run();
+
+        (new TestResponse($app->get(Http::class)->getResponse()))
+            ->isStatusCode(500)
+            ->isContentType('text/plain; charset=utf-8')
+            ->isBodySame('Custom message');
+    }    
+    
+    public function testAnyExceptionWithMessageJsonResponse()
+    {
+        $app = $this->createApp(request: [
+            'method' => 'GET',
+            'uri' => '',
+            'serverParams' => [],
+        ], accept: 'application/json');
+        
+        $app->middleware(function($request, $handler) {
+            throw new \RuntimeException();
+        });
+                
+        $app->run();
+
+        (new TestResponse($app->get(Http::class)->getResponse()))
+            ->isStatusCode(500)
+            ->isContentType('application/json')
+            ->isBodySame('{"status":500,"message":"Custom message"}');
+    }
+}
---
+++ tests/Mock/CustomErrorHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+ 
+namespace Tobento\App\Http\Test\Mock;
+
+use Tobento\App\Http\Boot\ErrorHandler;
+use Tobento\Service\Requester\RequesterInterface;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class CustomErrorHandler extends ErrorHandler
+{
+    public const INFO = [
+        'boot' => [
+            'Custom Error Handler',
+        ],
+    ];
+    
+    protected const HANDLER_PRIORITY = 3000;
+    
+    public function handleThrowable(Throwable $t): Throwable|ResponseInterface
+    {
+        $requester = $this->app->get(RequesterInterface::class);
+        
+        if ($t instanceof \RuntimeException) {
+            return $requester->wantsJson()
+                ? $this->renderJson(code: 500, message: 'Custom message')
+                : $this->renderView(code: 500, message: 'Custom message');
+        }
+        
+        return parent::handleThrowable($t);
+    }
+}
